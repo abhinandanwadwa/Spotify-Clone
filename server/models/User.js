88e8392f224ab656cf83d037acd73adcf030067a
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-08 17:03:46 +0530

    Creating User Schema

---
+++ server/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    imageURI: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('user', UserSchema);
