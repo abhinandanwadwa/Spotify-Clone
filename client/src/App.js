c701e819b19d2f1187c786a9684e6e02b824e4e7
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-24 18:49:18 +0530

    configuring Tailwind CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-  <Routes>
-    <Route path="/" element={ <Login/> } />
-  </Routes>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={ <Login/> } />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
