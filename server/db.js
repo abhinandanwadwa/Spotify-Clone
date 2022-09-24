7a4ced0f484bf5a7d5abe4e6e33ac908850e5adf
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-24 20:25:18 +0530

    Changing the db to connect to

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017";
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, { dbName: 'discordClone' }, () => {
+    mongoose.connect(mongoURI, { dbName: 'spotifyClone' }, () => {
         console.log("Connected To Mongo Successfully!!");
     });
 }
